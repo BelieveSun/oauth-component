d7af83c961b83758e723e86aef18ab00a5775ba2
sun.gj <sun.gj@finnovate.com.cn>
2017-07-14 19:17:50 +0800

    Initial commit

---
+++ src/main/java/com/believe/sun/OauthComponentApplication.java
@@ -0,0 +1,12 @@
+package com.believe.sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthComponentApplication.class, args);
+	}
+}
